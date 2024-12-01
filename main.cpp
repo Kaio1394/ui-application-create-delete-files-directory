86757eff8ae6d3f9cc0498d3bc91b3af0c093f2c
Kaio <kaio.santiago.13@hotmail.com>
2024-12-01 11:51:33 -0300

    removed icon line

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoginWindow w;
-
-    w.setWindowIcon(QIcon("‪‪C:/Arquivos/bot.ico"));
     w.show();
     return a.exec();
 }
